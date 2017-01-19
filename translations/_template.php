942be999bd7f72046ea8e1edc9e8439da3716be3
David Maes <david@white.nl>
2017-01-19 13:49:07 +0100

    Remove the extention from the files locale

--- translations/_template.php
+++ translations/_template.php
@@ -6,7 +6,7 @@ use Exception;
 
 try {
     $output = craft()->db->createCommand()->select('translations')->from('translate')->where([
-        'locale' => basename(__FILE__)
+        'locale' => basename(__FILE__, '.php')
     ])->queryColumn();
 
     if (isset($output[0])) {
